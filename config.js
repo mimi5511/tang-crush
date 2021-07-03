c8854abac5e1eae6a4a287136dc06f250d72447e
mimi5511 <86862347+mimi5511@users.noreply.github.com>
2021-07-03 11:14:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: '贤哥!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introDesc: `来玩个游戏吧, 我问你答`,
+    btnIntro: '嘿嘿',
+    title: '第一个问题 😙',
+    desc: '你喜欢什么动物? ',
+    btnYes: '熊猫',
+    btnNo: '小猫咪',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: '我想你一定很忙，所以，你只看前三个字就好了',
+    mess: '🥰',
+    messDesc: '现在回去跟香香聊天吧 ！',
+    btnAccept: '好啊<3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
